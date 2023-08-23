6ef99f6e0728c12b56fd6b490512d28479c6dc91
DrCommand <t0nk14.wib@gmail.com>
2023-08-24 05:56:16 +0700

    dev: do not touch yet

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const core = require('@actions/core')
 const httpm = require('@actions/http-client')
+const qs  = require('qs')
 
 async function run() {
   try {
@@ -18,21 +19,42 @@ async function run() {
     })
     core.info('Created HTTP client')
 
+    // Send a POST request (login)
     try {
-      let body = {
+      let data = {
         UserName: inputs.email,
         Password: inputs.password
       }
-      let res = await client.postJson(url, body)
+      let res = await client.post(url + `?${qs.stringify(data, { encode: false })}`)
       core.info('POST request sent')
 
       if (res.message.statusCode != 200) {
         throw new Error(`Request failed with status code ${res.message.statusCode}`)
+      } else if (res.message.headers['set-cookie'].length < 2) {
+        throw new Error('Login failed')
       }
-
     } catch (error) {
       throw new Error(`failed to post to: ${error.message}`)
     }
+    core.info('Login successful')
+
+    // Send a GET request (get license)
+    // try {
+    //   let data = {
+    //     UserName: inputs.email,
+    //     Password: inputs.password
+    //   }
+    //   let res = await client.post(url, JSON.stringify(data))
+    //   core.info('POST request sent')
+
+    //   if (res.message.statusCode != 200) {
+    //     throw new Error(`Request failed with status code ${res.message.statusCode}`)
+    //   } else if (res.message.headers['set-cookie'].length < 2) {
+    //     throw new Error('Login failed')
+    //   }
+    // } catch (error) {
+    //   throw new Error(`failed to post to: ${error.message}`)
+    // }
 
   } catch (error) {
     core.setFailed(error.message)
