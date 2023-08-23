aae0be28b9809618487cabc696f7fd9c34dacc51
DrCommand <t0nk14.wib@gmail.com>
2023-08-24 03:34:20 +0700

    build

--- dist/index.js
+++ dist/index.js
@@ -2862,7 +2862,30 @@ async function run() {
       password: core.getInput('cunet-password')
     }
 
-    core.info(`Logging in with ${inputs.email}`)
+    const url = 'https://licenseportal.it.chula.ac.th'
+
+    // Create a HTTP client
+    const client = new httpm.HttpClient('cunet-login-action', [], {
+      allowRetries: true,
+      maxRetries: 3
+    })
+    core.info('Created HTTP client')
+
+    try {
+      let body = {
+        UserName: inputs.email,
+        Password: inputs.password
+      }
+      let res = await client.postJson(url, body)
+      core.info('POST request sent')
+
+      if (res.message.statusCode != 200) {
+        throw new Error(`Request failed with status code ${res.message.statusCode}`)
+      }
+
+    } catch (error) {
+      throw new Error(`failed to post to: ${error.message}`)
+    }
 
   } catch (error) {
     core.setFailed(error.message)
