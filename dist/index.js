54f077b65eef7112ddee9de1a0ae154c4dafa91d
Tonkla W <t0nk14.wib@gmail.com>
2023-08-24 12:26:06 +0700

    update: v1.0.1

--- dist/index.js
+++ dist/index.js
@@ -11472,7 +11472,7 @@ async function run() {
     const inputs = {
       email: core.getInput('cunet-email'),
       password: core.getInput('cunet-password'),
-      licenseID: core.getInput('licenseID')
+      licenseID: core.getInput('license-id')
     }
 
     const url = 'https://licenseportal.it.chula.ac.th'
@@ -11548,7 +11548,7 @@ async function run() {
     }
 
   } catch (error) {
-    throw new Error(error.message)
+    core.setFailed(error.message)
   }
 }
 
