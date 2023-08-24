d074fedf588f8a3bdb06c7e23674b615b3587a48
Tonkla W <t0nk14.wib@gmail.com>
2023-08-24 13:21:26 +0700

    fix+update: updated to v1.0.2 with minor fixes

--- main.js
+++ main.js
@@ -3,13 +3,17 @@ const httpm = require('@actions/http-client')
 const qs = require('qs')
 const moment = require('moment-timezone')
 
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
+
 async function run() {
   try {
 
     const inputs = {
-      email: core.getInput('cunet-email'),
-      password: core.getInput('cunet-password'),
-      licenseID: core.getInput('license-id')
+      email: core.getInput('cunet-email') || process.env.CUNET_EMAIL,
+      password: core.getInput('cunet-password') || process.env.CUNET_PASSWORD,
+      licenseID: core.getInput('license-id') || 5
     }
 
     const url = 'https://licenseportal.it.chula.ac.th'
@@ -65,13 +69,13 @@ async function run() {
 
     // Send a POST request (get license)
     try {
-      const date = moment().tz('Asia/Bangkok').locale('th')
+      const date = moment().tz('Asia/Bangkok')
     
       data = qs.stringify({
         Domain: inputs.email.split('@')[1],
         ProgramLicenseID: inputs.licenseID,
-        BorrowDateStr: date.format('L'),
-        ExpiryDateStr: date.add(7, 'days').format('L'),
+        BorrowDateStr: date.format('DD/MM/YYYY'),
+        ExpiryDateStr: date.add(7, 'days').format('DD/MM/YYYY'),
         UserPrincipalName: inputs.email
       })
       let res = await client.post(url + `/Home/Borrow?${data}`, '', {
